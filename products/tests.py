37d63e16c0aecbb71d750b188e23089637d5ebcc
Cha764 <chattan.singh707@gmail.com>
2024-10-25 22:32:03 +0530

    Second commit

---
+++ products/tests.py
@@ -0,0 +1,71 @@
+from django import urls
+from django.conf.urls import url
+from django.http import response
+from django.test import client
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from .models import Product, User
+import json
+
+
+class ProductTests(APITestCase):
+    def test_create_product(self):
+        url = '/api/products/'
+        data = {
+            "title" : "Book test cases",
+            "image" : "book test cases image"
+        }
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Product.objects.count(), 1)
+        self.assertEqual(Product.objects.get().title, 'Book test cases')
+
+
+    def test_retrieve_product(self):
+        product = Product.objects.create(title='Book test cases1', image='book test cases image1')
+        product.save()
+
+        url = '/api/products/{0}'.format(product.id)
+        response = self.client.get(url)
+        result = json.loads(response.content)        
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(result['title'], product.title)
+    
+    
+    def test_list_products(self):
+        product = Product.objects.create(title='Book test cases1', image='book test cases image1')
+        product.save()
+        product = Product.objects.create(title='Book test cases2', image='book test cases image2')
+        product.save()
+
+        url = '/api/products/'
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Product.objects.count(), 2)    # test the 2 created objects
+
+
+    def test_delete_product(self):
+        product = Product.objects.create(title='Book test cases1', image='book test cases image1')
+        product.save()
+
+        url = '/api/products/{0}'.format(product.id)
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
+
+class UserTests(APITestCase):
+    def test_get(self):
+        user = User.objects.create(id=1)
+        user.save()
+
+        url = '/api/user/'
+        response = self.client.get(url)
+                
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(User.objects.count(), 1)    # test the created user object
---
+++ products/views.py
@@ -0,0 +1,68 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+from .models import Product, User
+from .serializers import ProductSerializer
+from rest_framework.views import APIView
+import random
+from .producer import publish
+
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+    def create(self, request):
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        publish('product-created', serializer.data)     # send message to the main(Flask) app
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+    def retrieve(self, request, pk=None):
+        try:
+            product = Product.objects.get(id=pk)
+            serializer = ProductSerializer(product)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Product.DoesNotExist:
+            return Response ({"Message" : "Product not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+    def update(self, request, pk=None):
+        try:
+            product = Product.objects.get(id=pk)
+            serializer = ProductSerializer(instance=product, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            publish('product-updated', serializer.data)     # send message to the main(Flask) app
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        except Product.DoesNotExist:
+            return Response ({"Message" : "Product not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+    def destroy(self, request, pk=None):
+        try:
+            product = Product.objects.get(id=pk)
+            product.delete()
+            product.save()
+            publish('product-deleted', pk)  # send message to the main(Flask) app
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Product.DoesNotExist:
+            return Response ({"Message" : "Product not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+class UserAPIView(APIView):
+    def get(self, _):
+        """
+        return a random user
+        """
+        users = User.objects.all()
+        user = random.choice(users)
+        return Response({
+            'id' : user.id
+        }, status=status.HTTP_200_OK)
